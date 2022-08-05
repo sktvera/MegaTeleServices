87c9d5000669a372dc0c37a4862114e812a7c847
veeraa1270@gmail.com <veeraa1270@gmail.com>
2022-08-05 12:28:47 -0500

    creando el app de react

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+//navegador de rutas
+import Navigation from "./navigation/Navigation";
+
+//retornando navegador de rutas
+const App = () => {
+  return <Navigation />;
+};
+
+export default App;
