6f29dff1725b93ac79595b801843d24121a769a0
veeraa1270@gmail.com <veeraa1270@gmail.com>
2022-11-01 17:58:52 -0500

    styles home

--- client/src/Components/WelcomeHome/WelcomeHome.jsx
+++ client/src/Components/WelcomeHome/WelcomeHome.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import './Assets/styles.css'
+import{ Button,Grid,Box }  from '@mui/material'
 
 function WelcomeHome() {
   return (
-    <div>
-        <div>
-          <h1>ww</h1>
-          <label>ww</label>
-          <button>ww</button>
-        </div>
+    <div className='containerWelcome'>
+        <Grid >
+              <Box className='itemWelcome'>
+                  <h1>The Best TV service</h1>
+                  <h1>around!</h1>
+              </Box>
+              
+              <Box className='itemWelcome'>
+                  <label>Are you ready to enjoy ultra-best service?</label>
+              </Box>
+              <Box className='itemWelcome'>
+                  <Button variant="contained" className='buttonWelcome'>Let's DO IT!</Button>
+              </Box>
+        </Grid>
     </div>
   )
 }
